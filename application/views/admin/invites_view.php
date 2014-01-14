e868bbfc0f835ea769a7f5cb002d9af94c91878f
Andrey Boboshko <an-03@yandex.ru>
2014-01-14 15:34:06 +0200

    added some fixes

--- application/views/admin/invites_view.php
+++ application/views/admin/invites_view.php
@@ -64,75 +64,67 @@
 	</div>
 	<div class="row">
 
-	<table id="allInvitesTable" class="tablesorter tablesorter-blue"><colgroup><col style="width: 11.9%;"><col style="width: 12.5%;"><col style="width: 12.1%;"><col style="width: 6.3%;"><col style="width: 8.2%;"><col style="width: 10.7%;"><col style="width: 12.2%;"><col style="width: 21.9%;"></colgroup>
+	<table id="allInvitesTable" class="tablesorter tablesorter-blue">
+		<colgroup>
+			<col style="width: 3%;">
+			<col style="width: 10%;">
+			<col style="width: 20%;">
+			<col style="width: 20%;">
+			<col style="width: 47%;">
+		</colgroup>
 		<thead>
 			<tr class="tablesorter-headerRow">
-				<th data-column="0" class="tablesorter-header tablesorter-headerAsc" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Account #</div></th>
-				<th data-column="1" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">First Name</div></th>
-				<th data-column="2" class="tablesorter-header tablesorter-headerDesc" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Last Name</div></th>
-				<th data-column="3" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Age</div></th>
-				<th data-column="4" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Total</div></th>
-				<th data-column="5" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Discount</div></th>
-				<th data-column="6" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Difference</div></th>
-				<th data-column="7" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Date</div></th>
+				<th data-column="0" class="tablesorter-header tablesorter-headerAsc" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">№</div></th>
+				<th data-column="1" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Пользователь</div></th>
+				<th data-column="2" class="tablesorter-header tablesorter-headerDesc" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Имя</div></th>
+				<th data-column="3" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Фамилия</div></th>
+				<th data-column="4" class="tablesorter-header" tabindex="0" unselectable="on" style="-webkit-user-select: none;"><div class="tablesorter-header-inner">Инвайт</div></th>
 			</tr>
 		</thead>
 		<tbody>
-		<tr class="odd">
-				<td>A1</td>
+			<tr class="odd">
+				<td>1</td>
 				<td>Bruce</td>
 				<td>Almighty</td>
 				<td>45</td>
 				<td>$153.19</td>
-				<td>44.7%</td>
-				<td>+77</td>
-				<td>Jan 18, 2001 9:12 AM</td>
-			</tr><tr class="even">
-				<td>A33</td>
+			</tr>
+			<tr class="even">
+				<td>2</td>
 				<td>Clark</td>
 				<td>Kent</td>
 				<td>18</td>
 				<td>$15.89</td>
-				<td>44%</td>
-				<td>-26</td>
-				<td>Jan 12, 2003 11:14 AM</td>
-			</tr><tr class="odd">
-				<td>A42a</td>
+			</tr>
+			<tr class="odd">
+				<td>3</td>
 				<td>Bruce</td>
 				<td>Evans</td>
 				<td>22</td>
 				<td>$13.19</td>
-				<td>11%</td>
-				<td>0</td>
-				<td>Jan 18, 2007 9:12 AM</td>
-			</tr><tr class="even">
-				<td>A42b</td>
+			</tr>
+			<tr class="even">
+				<td>4</td>
 				<td>Peter</td>
 				<td>Parker</td>
 				<td>28</td>
 				<td>$9.99</td>
-				<td>20.9%</td>
-				<td>+12.1</td>
-				<td>Jul 6, 2006 8:14 AM</td>
-			</tr><tr class="odd">
-				<td>A102</td>
+			</tr>
+			<tr class="odd">
+				<td>5</td>
 				<td>Bruce</td>
 				<td>Evans</td>
 				<td>22</td>
 				<td>$13.19</td>
-				<td>11%</td>
-				<td>-100.9</td>
-				<td>Jan 18, 2007 9:12 AM</td>
-			</tr><tr class="even">
-				<td>A255</td>
+			</tr>
+			<tr class="even">
+				<td>6</td>
 				<td>John</td>
 				<td>Hood</td>
 				<td>33</td>
 				<td>$19.99</td>
-				<td>25%</td>
-				<td>+12</td>
-				<td>Dec 10, 2002 5:14 AM</td>
-			</tr></tbody>
+			</tr>
+			</tbody>
 	</table>
 	
 	
--- assets/js/admin_view.js
+++ assets/js/admin_view.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
     });
     
     $("#allInvitesTable").tablesorter();
+    $("#allInvitesTable").hide();
 });
 
 
@@ -43,13 +44,23 @@ function create_invites() {
 	     data: postData ,
 	     success: function(msg, status){
 	    	 console.log(msg);
-	    	 console.log(msg[0][19][1]);
+	    	 //console.log(msg[0][19][1]);
 	    	 
 	   	     $.bootstrapGrowl("Создано", { type: 'success',
 		    	     ele: 'body', 
 		    	     align: 'center',
 		    	     delay: 300
 		    	    	 });
+	   	  
+	   	     var row = '<tr class="even"><td>7</td><td>Vasya</td><td>Hood</td><td>33</td><td>$19.99</td></tr>',
+	   	     
+	   	     $row = $(row),
+		      // resort table using the current sort; set to false to prevent resort, otherwise 
+		      // any other value in resort will automatically trigger the table resort. 
+		      resort = true;
+	   	     $("#allInvitesTable").find('tbody').append($row);
+	   	     $("#allInvitesTable").trigger('addRows', [$row, resort]);
+	   	     $("#allInvitesTable").show();
 	     },
 	     error: function (e, data) {
 	    	 alert(JSON.stringify(error));
